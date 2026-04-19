4042d61c37a132e9c86c87035446268a275ef5f4
Manuel Losa Tejero <manuelosauk@gmail.com>
2026-04-19 13:17:19 +0100

    Refactored code: input validation, functions, dictionary menu

--- main.py
+++ main.py
@@ -1,27 +1,54 @@
 from logic import analyze_age, analyze_income, analyze_savings 
 
+def get_number(prompt):
+    while True:
+        value = input(prompt)
+
+        if value.isdigit():
+            return int(value)
+        else:
+            print("Please enter a valid number.")
+
+def show_result(label, result):
+    print(label, result)
+
+
+
+options = [
+    "1. Check income",
+    "2. Check age",
+    "3. Check savings",
+    "4. Exit"
+]
+
+def handle_income():
+    income = get_number("Income: ")
+    show_result("Income:", analyze_income(income))
+
+def handle_age():
+    age = get_number("Age: ")
+    show_result("Age:", analyze_age(age))
+
+def handle_savings():
+    savings = get_number("Savings: ")
+    show_result("Savings:", analyze_savings(savings))
+
+actions = {
+    "1": handle_income,
+    "2": handle_age,
+    "3": handle_savings
+}
 
 def main ():
     while True:
         print("\n=== DASHBOARD ===")
-        print("1. Check income")
-        print("2. Check age")
-        print("3. Check savings")
-        print("4. Exit")
+        for option_text in options:
+            print(option_text)
 
         option = input("Choose: ")
 
-        if option == "1":
-            income = int(input("Income: "))
-            print(analyze_income(income))
-
-        elif option == "2":
-            age = int(input("Age: "))
-            print(analyze_age(age))
-
-        elif option == "3":
-            savings = int(input("Savings: "))
-            print(analyze_savings(savings))
+        if option in actions:
+            actions[option]()
 
         elif option == "4":
             print("Good bye")
@@ -30,4 +57,5 @@ def main ():
         else:
             print("Invalid option")
 
-main()
+if __name__ == "__main__":
+    main()
