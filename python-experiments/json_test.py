fbd837751f453c34059719ad80777692c04c9efa
Manuel Losa Tejero <manuelosauk@gmail.com>
2026-05-23 15:23:16 +0100

    Practised JSON files and Python data conversion

--- python-experiments/json_test.py
+++ python-experiments/json_test.py
@@ -14,3 +14,18 @@ python_task = json.loads(json_task)
 
 print(python_task)
 print(type(python_task))
+
+
+tasks = [
+    {"title": "Gym", "done": False},
+    {"title": "Study", "done": True}
+]
+
+with open("tasks.json", "w") as file:
+    json.dump(tasks, file)
+
+with open("tasks.json", "r") as file:
+    loaded_tasks = json.load(file)
+
+print(loaded_tasks)
+print(type(loaded_tasks))
